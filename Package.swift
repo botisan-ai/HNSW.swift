549e4c419e045a919a26053e3ab82028af7aa932
Simon Liang <simon@x-tech.io>
2026-02-06 13:11:30 +0800

    Release 0.2.1 with universal iOS-sim/macOS XCFramework build

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription
 
-let useLocalFramework = false
+let useLocalFramework = true
 let binaryTarget: Target
 
 if useLocalFramework {
@@ -11,8 +11,8 @@ if useLocalFramework {
         path: "./build/libhnsw-rs.xcframework"
     )
 } else {
-    let releaseTag = "0.2.0"
-    let releaseChecksum = "9c9cef077cee73af1d084e629c56893a799ece28fc8a39b6adc95e6465f7c786"
+    let releaseTag = "0.2.1"
+    let releaseChecksum = "1cd0aabd6a26625259673df11709b2f29f568e5b4391d4a5f9c0ac531d9986cf"
     binaryTarget = .binaryTarget(
         name: "HnswRS",
         url:
